2e04b2994e6167cdd1c0bdb736cd922c50319354
Shyam Kumar Sharma <136264959+Shyamkumar1234@users.noreply.github.com>
2024-09-08 10:08:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.static("public"))
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/techmeet/home", (req, res)=>{
+app.get("/", (req, res)=>{
     res.render("home.ejs");
 })
 app.get("/techmeet/:username", (req, res)=>{
